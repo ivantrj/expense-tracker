377ef432c2559d9aab01eb35a721d61153b44de0
Ivan Trajanovski ruwido <ivan.trajanovski@ruwido.com>
2023-11-29 09:26:33 +0100

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by ivan ruwido  on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
