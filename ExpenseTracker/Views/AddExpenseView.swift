77841a42f8c87c70afe4266281049727492cce32
ivan <ivantrajanovski@gmail.com>
2023-11-29 10:11:32 +0100

    create add expense view

--- ExpenseTracker/Views/AddExpenseView.swift
+++ ExpenseTracker/Views/AddExpenseView.swift
@@ -8,8 +8,89 @@
 import SwiftUI
 
 struct AddExpenseView: View {
+    
+    @State private var name = ""
+    @State private var amount = ""
+    @State private var isExpenseAdded = false
+    @State private var selectedCategory: String? = nil
+    
+    let categories = ["Food", "Housing", "Transport", "Entertainment", "Utilities"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20) {
+                TextField("Name of Expense", text: $name)
+                    .padding()
+                    .foregroundStyle(.secondary)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                
+                TextField("Amount", text: $amount)
+                    .padding()
+                    .foregroundStyle(.secondary)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                
+                LazyVGrid(columns: [
+                    GridItem(.flexible(), spacing: 16),
+                    GridItem(.flexible(), spacing: 16),
+                    GridItem(.flexible(), spacing: 16)
+                ], spacing: 16) {
+                    ForEach(categories[0...2], id: \.self) { category in
+                        Button {
+                            selectedCategory = category
+                        } label: {
+                            Text(category)
+                                .foregroundStyle(selectedCategory == category ? .white : .black)
+                                .bold()
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(selectedCategory == category ? .blue : .white)
+                                .cornerRadius(10)
+                                .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                        }
+}
+                }
+                
+                LazyVGrid(columns: [
+                    GridItem(.flexible(), spacing: 16),
+                    GridItem(.flexible(), spacing: 16)
+                ], spacing: 16) {
+                    ForEach(categories[3...4], id: \.self) { category in
+                        Button {
+                            selectedCategory = category
+                        } label: {
+                            Text(category)
+                                .foregroundStyle(selectedCategory == category ? .white : .black)
+                                .bold()
+                                .frame(maxWidth: .infinity, minHeight: 50)
+                                .background(selectedCategory == category ? .blue : .white)
+                                .cornerRadius(10)
+                                .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                        }
+}
+                }
+                
+                Button {
+                    //
+                } label: {
+                    Text("Add Expense")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .frame(maxWidth: .infinity, minHeight: 50)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                }
+                .padding()
+                
+                Spacer()
+
+            }
+            .padding(.horizontal)
+            .navigationTitle("Add Expense")
+        }
     }
 }
 
